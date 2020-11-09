93c8ebcfc9103e6b1a83d3c595c549fd2bde6405
xarchie25 <gartur25@o2.pl>
2020-11-09 23:12:46 +0100

    Setup client & seed activities

---
+++ Persistence/Seed.cs
@@ -0,0 +1,14 @@
+using System.Linq;
+
+namespace Persistence
+{
+    public class Seed
+    {
+        public static void SeedData(DataContext context)
+        {
+            if(!context.Activities.Any()){
+                var activities = new List<Activity>
+            }
+        }
+    }
+}
